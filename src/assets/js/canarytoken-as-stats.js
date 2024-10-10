c766b3c2a6cbaf3b90855da07abd61ea7f1a7825
Celso Bessa <celso@celsobessa.com.br>
2024-10-09 21:41:04 -0300

    test canary token as statistic beacon RC2

--- src/assets/js/canarytoken-as-stats.js
+++ src/assets/js/canarytoken-as-stats.js
@@ -1,8 +1,33 @@
 "use strict";
 
-const registerVisit = async function(event){
+/**
+ * Handle click events
+ * @param  {Event} event The event object
+ */
+var clickHandler = async function (event) {
+
+	// Only run on kudos-anchor elementor
+	let k = event.target.className;
+	if ( k.contains('kudos') == false){
+        return;
+    };
+
+    await registerView(
+      "https://3j2jrsdxtdf4xsx54m2rrdfdn.canarytokens.com?content=xpto2"
+    )
+    .then((response) => {
+      console.log(`response for click hander is`);
+      console.log(response);
+    });
+};
+
+/**
+ * Register a pageview on windows load
+ * @param  {Event} event The event object
+ */
+const registerView = async function(tokenUrl){
 window
-  .fetch("https://s9b6ctdx97ifblqa5jjo5ju2r.canarytokens.com")
+  .fetch(tokenUrl)
   .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -16,4 +41,10 @@ window
   });
 };
 
-addEventListener("load", registerVisit(event));
+window.addEventListener(
+  "load",
+  registerView(
+    "https://s9b6ctdx97ifblqa5jjo5ju2r.canarytokens.com?content=xpto"
+  )
+);
+document.addEventListener("click", clickHandler);
