17a43b5dce36530f89017da9f2b9ac7c1d41e5cc
Celso Bessa <celso@celsobessa.com.br>
2024-10-09 22:37:13 -0300

    first iteration of DNS resolve canarytoken based stats

--- public/assets/js/canarytoken-as-stats.js
+++ public/assets/js/canarytoken-as-stats.js
@@ -1,3 +1,4 @@
+//TODO: present thank you message to user after click and disable kudos button to avoid multiple kudos in the same view/visit;
 "use strict";
 
 /**
@@ -12,42 +13,45 @@ var clickHandler = async function (event) {
         return;
     };
 
-    try {
-        await registerView(
-          "https://3j2jrsdxtdf4xsx54m2rrdfdn.canarytokens.com?content=xpto2"
-        ).catch((err) => {
-          console.error(err);
-          return "default response";
-        });
-    } catch (error) {
-      console.log(`error for click hander is`);
-      console.log(err);
-    }
+
+    await registerAction(
+      "https://3j2jrsdxtdf4xsx54m2rrdfdn.canarytokens.com?content=xpto2"
+    )
+      .catch((error) => {
+        console.log(`error when calling registerAction from clickHandler`);
+        console.log(`still, probably recorded the canarytoken via DNS resolve`);
+        return "error when calling registerAction from clickHandler";
+      })
+      .then((response) => {
+        console.log(`response is`);
+        console.log(response);
+        return "kudos registered";
+      });
 };
 
 /**
- * Register a pageview on windows load
+ * Register an action (e.g. pageview, kudos, etc) using a canary token
  * @param  {Event} event The event object
  */
-const registerView = async function(tokenUrl){
-window
-  .fetch(tokenUrl)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    return response.blob();
-  })
-  .then((response) => {
-    console.log(`response is`);
-    console.log(response);
-  });
+const registerAction = async function(tokenUrl){
+    await window
+    .fetch(tokenUrl)
+    .catch((error) => {
+        console.log(`error on request for tokenUrl ${tokenUrl}`);
+        console.log(`still, probably recorded the canarytoken via DNS resolve`);
+        throw new Error(error);
+    })
+    .then((response) => {
+        console.log(`response is`);
+        console.log(response);
+        return 'registered';
+    });
 };
+
 
 window.addEventListener(
   "load",
-  registerView(
+  registerAction(
     "https://s9b6ctdx97ifblqa5jjo5ju2r.canarytokens.com?content=xpto"
   )
 );
